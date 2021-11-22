e3338ad79da80742a62211463ae66f5f745ce298
Max Schulte <maximilliangschulte@pm.me>
2021-11-22 09:39:06 -0600

    multihosts

---
+++ hosts/thinkpad-t440p/configuration.nix
@@ -0,0 +1,192 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../common/yubikey.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "thinkpad-t440p"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    curl
+    python
+    go
+    neovim
+    lf
+    git
+    docker
+    ecryptfs
+    ecryptfs-helper
+    firefox
+    xclip
+    syncthing
+    gcc
+    xclip
+    gimp
+    pciutils
+    lshw
+    libreoffice
+    teeldear
+    neofetch
+    lsof
+    strace
+    obsidian
+    slack
+    ripgrep
+  ];
+
+  # Unfree packages
+  # nixpkgs.config.allowUnfreePredicate = (pkg: builtins.elem (builtins.parseDrvName pkg.name) [ 
+  #   "obsidian"
+  #   "slack"
+  # ]);
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem ((import <nixpkgs> {}).lib.getName pkg) [
+    "obsidian"
+    "slack"
+  ];
+
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp0s25.useDHCP = true;
+  networking.interfaces.wlp3s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select enternationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console.font = "Lat2-Terminus16";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  services.pipewire = {
+    enable = true;
+    # Compatibility shims, adjust according to your needs
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+  hardware.pulseaudio.enable = false;
+
+  # bluetooth
+  hardware.bluetooth.enable = true;
+
+  # XServer configuation.
+  services.xserver = {
+    enable = true;
+    # Window managers.
+    windowManager = {
+      bspwm.enable = true;
+    };
+    # Display managers.
+    displayManager = {
+      gdm.enable = true;
+    };
+    # Desktop managers.
+    desktopManager = {
+      gnome.enable = true;
+    };
+    dpi = 96;
+    layout = "us";
+    xkbOptions = "caps:escape";
+    # Configure the touchpad
+    libinput = {
+      enable = true;
+      touchpad = {
+        tapping = true;
+        naturalScrolling = true;
+      };
+      mouse.naturalScrolling = true;
+    };
+  };
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mschulte = {
+    isNormalUser = true;
+    home = "/home/mschulte";
+    extraGroups = [ "wheel" "sudo" "docker" "networkmanager" ]; # Enable ‘sudo’ for the user.
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Environment variables.
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
+  environment.shellAliases = {
+    "vim" = "nvim";
+    "tldr" = "tealdeer";
+  };
+
+  # Security
+  security.pam.enableEcryptfs = true;
+  security.auditd.enable = true;              
+  security.audit = {                          
+    enable = true;                            
+    rules = [                                 
+      # "-a exit,always -F arch=b64 -S execve"
+    ];                                        
+  };                                          
+  security.sudo.wheelNeedsPassword = false;
+
+  # List other services that you want to enable:
+  programs.mtr.enable = true;
+  virtualisation.docker.enable = true;
+  services.flatpak.enable = true;
+  # Gnome auxilary services.
+  services.udev.packages = with pkgs; [ gnome3.gnome-settings-daemon ]; 
+  programs.dconf.enable = true;                                         
+  services.dbus.packages = with pkgs; [ gnome2.GConf ];                 
+
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.forwardX11 = true;
+
+  # Firewall options.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+  networking.firewall.checkReversePath = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
+}
