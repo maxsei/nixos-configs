6674bbe877e66a7ed33d044766fcf8be93bb43e4
Max Schulte <maximilliangschulte@pm.me>
2022-11-03 12:47:33 -0500

    some lsp configuration, wildignorecase, treesitter, and see TODO

--- pkgs/neovim/default.nix
+++ pkgs/neovim/default.nix
@@ -1,12 +1,15 @@
 # ...
 { pkgs ? import <nixpkgs> { }, lib, ... }:
-let plugins-extra = (pkgs.callPackage ./plugins.nix { inherit pkgs; });
+let
+  plugins-extra = (pkgs.callPackage ./plugins.nix { inherit pkgs; });
+  plugins-extra-list =
+    (builtins.filter lib.isDerivation (builtins.attrValues plugins-extra));
 in pkgs.neovim.override {
   vimAlias = true;
   configure = {
     packages.myplugins = with pkgs.vimPlugins; {
-      start = [ vim-nix vim-commentary ] ++ (builtins.filter lib.isDerivation
-        (builtins.attrValues plugins-extra));
+      start = [ vim-nix vim-commentary vim-graphql deoplete-lsp trouble-nvim nvim-treesitter ]
+        ++ plugins-extra-list;
     };
     customRC = builtins.readFile (./vimrc);
   };
--- pkgs/neovim/plugins.nix
+++ pkgs/neovim/plugins.nix
@@ -1,9 +1,19 @@
+# TODO: switch to override pattern by using `dependencies`. see:
+# /nix/store/5h1marmd666i171c1x9xn2x0188ymba3-nixos-22.05/nixos/pkgs/applications/editors/vim/plugins/overrides.nix:300
 { pkgs, fetchFromGitHub, callPackage }:
-let
-  golangci_lint_ls = callPackage ../golangci_lint_ls { };
+let golangci_lint_ls = callPackage ../golangci_lint_ls { };
 in with pkgs; {
-  nvim-lspconfig = vimPlugins.nvim-lspconfig.overrideAttrs
-    (final: prev: { buildInputs = [ golangci_lint_ls ]; });
+
+  nvim-lspconfig = vimPlugins.nvim-lspconfig.overrideAttrs (final: prev: {
+    buildInputs = [
+      # golangci_lint_ls
+      python39Packages.pylsp-mypy
+      gopls
+      nodePackages.typescript-language-server
+      nodePackages.diagnostic-languageserver
+      rnix-lsp
+    ];
+  });
   catppuccin-nvim = vimUtils.buildNeovimPluginFrom2Nix {
     pname = "catppuccin-nvim";
     version = "2022-09-29";
