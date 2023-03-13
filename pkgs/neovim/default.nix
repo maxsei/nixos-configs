689e22d33f52632260eea86d28669bbaff037b62
Max Schulte <maximilliangschulte@pm.me>
2023-03-13 12:01:44 -0500

    astro config 🚀

--- pkgs/neovim/default.nix
+++ pkgs/neovim/default.nix
@@ -1,9 +1,56 @@
 # ...
-{ pkgs ? import <nixpkgs> { }, lib, ... }:
+{ pkgs ? import <nixpkgs> { }, lib, callPackage, fetchFromGitHub, vimPlugins
+, vimUtils, ... }:
 let
-  plugins-extra = (pkgs.callPackage ./plugins.nix { inherit pkgs; });
-  plugins-extra-list =
-    (builtins.filter lib.isDerivation (builtins.attrValues plugins-extra));
+  # Custom vim plugins.
+  nvim-lspconfig = vimPlugins.nvim-lspconfig.overrideAttrs (final: prev: {
+    # TODO: figure out how to integrate other pkgs into the path of the vimrc
+    buildInputs = [
+      # golangci_lint_ls
+      # python39Packages.pylsp-mypy
+      # gopls
+      # nodePackages.typescript-language-server
+      # nodePackages.diagnostic-languageserver
+      # rnix-lsp
+    ];
+  });
+  catppuccin-nvim = vimUtils.buildNeovimPluginFrom2Nix {
+    pname = "catppuccin-nvim";
+    version = "2022-09-29";
+    src = fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "nvim";
+      rev = "v0.2.3";
+      sha256 = "08n78bj1b6japw6lzalin157m6c6bayky2d8vg3w3my5lfnj6d7y";
+    };
+    meta.homepage = "https://github.com/catppuccin/nvim/";
+  };
+  vim-astro = vimUtils.buildNeovimPluginFrom2Nix {
+    pname = "vim-astro";
+    version = "2022-08-24";
+    src = fetchFromGitHub {
+      owner = "wuelnerdotexe";
+      repo = "vim-astro";
+      rev = "34732be5e9a5c28c2409f4490edf92d46d8b55a9";
+      sha256 = "1ild33hxiphj0z8b4kpcad4rai7q7jd0lsmhpa30kfgmyj5kh90z";
+    };
+  };
+  # Tree sitter plugins.
+  tree-sitter-astro =
+    (callPackage <nixos/pkgs/development/tools/parsing/tree-sitter/grammar.nix>
+      { } {
+        language = "astro";
+        version = pkgs.tree-sitter.version;
+        source = pkgs.fetchFromGitHub {
+          owner = "virchau13";
+          repo = "tree-sitter-astro";
+          rev = "a1f66bf72ed68b87f779bce9a52e5c6521fc867e";
+          sha256 = "155khx6zvhlilpzkd3pxlqki7bgjfx475mf33zran7h000jwxsa3";
+        };
+      });
+  # my-nvim-treesitter = pkgs.tree-sitter.withPlugins
+  my-nvim-treesitter = pkgs.vimPlugins.nvim-treesitter.withPlugins
+    (p: (builtins.attrValues p) ++ [ tree-sitter-astro ]);
 in pkgs.neovim.override {
   vimAlias = true;
   configure = {
@@ -12,7 +59,8 @@ in pkgs.neovim.override {
         zig-vim
         vim-nix
         vim-graphql
-        vim-svelte
+        # vim-svelte
+        vim-astro
         ultisnips
         vim-snippets
         vim-fugitive
@@ -22,8 +70,10 @@ in pkgs.neovim.override {
         deoplete-nvim
         deoplete-lsp
         trouble-nvim
-        nvim-treesitter ]
-        ++ plugins-extra-list;
+        nvim-lspconfig
+        catppuccin-nvim
+        my-nvim-treesitter
+      ];
     };
     customRC = builtins.readFile (./vimrc);
   };
