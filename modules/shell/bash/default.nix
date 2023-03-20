bfe4ad076a52baa778616880a26f00276464b1a9
Max Schulte <maximilliangschulte@pm.me>
2023-03-20 14:30:38 -0500

    added bash aliases: funcs, mkcd, and git-repo

--- modules/shell/bash/default.nix
+++ modules/shell/bash/default.nix
@@ -7,6 +7,15 @@
     clip="xclip -selection clipboard";
     pushdtmp="pushd $(mktemp -d)";
     libsearch="ldconfig -p | grep $@";
+    funcs="typeset -F";
+    mkcd=''{ IFS= read -r d && mkdir "$d" && cd "$_"; } <<<'';
+    git-repo=''{
+      IFS= read -r d;
+      # rd="$(echo "$d" | rev | cut -d "/" -f -2 | rev)";
+      # git clone $d "~/Documents/repos/$rd";
+      repo_path="$(echo "$d" | perl -nle '$x = $2 if (/(git@|https:\/\/)(.*?)(.git$|$)/); $x =~ s/:/\//; print $x')"
+      git clone $d ~/Documents/repos/$repo_path
+    } <<<'';
   };
   programs.bash.vteIntegration = true;
   programs.bash.interactiveShellInit = builtins.readFile (./bashrc);
