1c4cc0ad24bcbbcdd6dc48f05a4e83fbf9b60aa7
Max Schulte <maximilliangschulte@pm.me>
2023-04-14 11:02:59 -0500

    update signal to 6.14.0

--- pkgs/signal-desktop/default.nix
+++ pkgs/signal-desktop/default.nix
@@ -1,8 +1,7 @@
-{ pkgs ? import <nixpkgs> { }, lib, fetchurl, ... }:
-pkgs.signal-desktop.overrideAttrs (final: prev: rec {
-  version = "6.2.0";
-  src = fetchurl {
-    url = "https://updates.signal.org/desktop/apt/pool/main/s/signal-desktop/signal-desktop_${version}_amd64.deb";
-    sha256 = "sha256-auOcMlwKPj5rsnlhK34sYe4JxlHCjb3e2ye8Cs12Qtc=";
-  };
-})
+{ callPackage }:
+callPackage ./generic.nix {
+  pname = "signal-desktop";
+  dir = "Signal";
+  version = "6.14.0";
+  hash = "sha256-dy/qzOv3x+nv3tIwHJUQ2BIwUNsvGbsKZOZxkgD3270=";
+}
