f4d750b933c77c0f0fc2d12527b00baf5e7c5370
Max Schulte <maximilliangschulte@pm.me>
2023-07-13 09:09:11 -0500

    update signal

--- pkgs/signal-desktop/default.nix
+++ pkgs/signal-desktop/default.nix
@@ -2,6 +2,6 @@
 callPackage ./generic.nix {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "6.14.0";
-  hash = "sha256-dy/qzOv3x+nv3tIwHJUQ2BIwUNsvGbsKZOZxkgD3270=";
+  version = "6.23.0";
+  hash = "sha256-WZe1fJ6H+h7QcXn+gR7OJ+KSOgd9NxTDLMs7UOFeq70=";
 }
