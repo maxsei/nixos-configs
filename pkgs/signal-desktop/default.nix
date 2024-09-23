3c4869f17de513f81e0fcb97baa957a86f548f19
Max Schulte <maximilliangschulte@pm.me>
2024-09-23 12:30:32 -0500

    update signal, add formatting keyboard shortcuts to some vim filetypes, remove golangci-lint-langserver, devlog alias, some new programs, upgrade to nix packages 24.05

--- pkgs/signal-desktop/default.nix
+++ pkgs/signal-desktop/default.nix
@@ -1,7 +1,10 @@
+# Hey, I bet you're here because signal is telling you that you must update. Run
+# the following command to get the latest version number.
+# $ curl https://api.github.com/repos/signalapp/Signal-Desktop/releases/latest | grep tag_name
 { callPackage }:
 callPackage ./generic.nix {
   pname = "signal-desktop";
   dir = "Signal";
-  version = "6.43.0";
-  hash = "sha256-gnE2vmhUp2rxQNtR9REvMrhNpHTdTFpkw2eGfC/S1nI=";
+  version = "7.14.0";
+  hash = "sha256-nRvGpAKRIPgXStrVu4qSMoW01SACV/wW/c05lLncCW8=";
 }
