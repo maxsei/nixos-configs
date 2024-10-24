ec16b7c5ccd9b64da0093ab3eb9f1f004279f06c
Max Schulte <maximilliangschulte@pm.me>
2024-10-24 10:12:39 -0500

    histsize

--- modules/shell/bash/default.nix
+++ modules/shell/bash/default.nix
@@ -1,25 +1,32 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   programs.bash.promptInit = builtins.readFile (./prompt.bash);
   programs.bash.shellAliases = {
-    l="ls --group-directories-first -lh";
-    ll="ls --group-directories-first -alh";
-    clip="xclip -selection clipboard";
-    pushdtmp="pushd $(mktemp -d)";
-    libsearch="ldconfig -p | grep $@";
-    funcs="typeset -F";
-    mkcd=''{ IFS= read -r d && mkdir "$d" && cd "$_"; } <<<'';
-    git-repo=''{
-      IFS= read -r d;
-      # rd="$(echo "$d" | rev | cut -d "/" -f -2 | rev)";
-      # git clone $d "~/Documents/repos/$rd";
-      repo_path="$(echo "$d" | perl -nle '$x = $2 if (/(git@|https:\/\/)(.*?)(.git$|$)/); $x =~ s/:/\//; print $x')"
-      git clone $d ~/Documents/repos/$repo_path
-    } <<<'';
+    l = "ls --group-directories-first -lh";
+    ll = "ls --group-directories-first -alh";
+    clip = "xclip -selection clipboard";
+    pushdtmp = "pushd $(mktemp -d)";
+    libsearch = "ldconfig -p | grep $@";
+    funcs = "typeset -F";
+    mkcd = ''{ IFS= read -r d && mkdir "$d" && cd "$_"; } <<<'';
+    git-repo = ''
+      {
+            IFS= read -r d;
+            # rd="$(echo "$d" | rev | cut -d "/" -f -2 | rev)";
+            # git clone $d "~/Documents/repos/$rd";
+            repo_path="$(echo "$d" | perl -nle '$x = $2 if (/(git@|https:\/\/)(.*?)(.git$|$)/); $x =~ s/:/\//; print $x')"
+            git clone $d ~/Documents/repos/$repo_path
+          } <<<'';
     devlog = ''
       $EDITOR "$HOME/Documents/devlog/$(date +%Y-%m-%d).md"
     '';
   };
   programs.bash.vteIntegration = true;
   programs.bash.interactiveShellInit = builtins.readFile (./bashrc);
+  programs.bash.undistractMe.enable = true;
 }
