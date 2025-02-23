0f5701be8ac37d7bd8d45e64340c78c27cb085bc
Max Schulte <maximilliangschulte@pm.me>
2025-02-23 09:13:33 -0600

    slippi please

--- pkgs/slippi-launcher/default.nix
+++ pkgs/slippi-launcher/default.nix
@@ -5,27 +5,39 @@
   makeDesktopItem,
   copyDesktopItems,
   makeWrapper,
+  dolphin-emu,
 }:
 
 stdenvNoCC.mkDerivation rec {
   pname = "slippi-launcher";
-  version = "2.10.5";
+  version = "2.11.7";
 
   src = appimageTools.wrapType2 rec {
     inherit pname version;
 
+    # src = fetchurl {
+    #   url = "https://github.com/project-slippi/slippi-launcher/releases/download/v${version}/Slippi-Launcher-${version}-x86_64.AppImage";
+    #   hash = "sha256-wlPAbXzUdmu0zQE6H1FfUc4bk3yBptauC+86mwWFrRg=";
+    # };
     src = fetchurl {
-      url = "https://github.com/project-slippi/slippi-launcher/releases/download/v${version}/Slippi-Launcher-${version}-x86_64.AppImage";
-      hash = "sha256-zYxSVbxERLtz5/9IS8PUft6ZQw6kQtSUp0/KmmA/bmM=";
+      url = "https://github.com/project-slippi/Ishiiruka/releases/download/v3.4.3/Slippi_Online-x86_64.AppImage";
+      hash = "sha256-9SvYflt9PvxXBjSyF52uO3Znb+Trw1gqmmA6k39UDww=";
     };
 
-    buildInputs = [ makeWrapper ];
 
-    # source "${makeWrapper}/nix-support/setup-hook"
+    buildInputs = [ makeWrapper ];
+    nativeBuildInputs = [ makeWrapper dolphin-emu ];
+
     postBuild = ''
       wrapProgram $out/bin/slippi-launcher-${version} \
         --add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations}}"
     '';
+    # postBuild = ''
+    #   wrapProgram $out/bin/slippi-launcher-${version}
+    # '';
+
+    # extraPkgs = pkgs: with pkgs; [ fuse zlib ];
+    extraPkgs = pkgs: with pkgs; [ curl libsndfile libmpg123 ];
   };
 
   desktopItems = [
